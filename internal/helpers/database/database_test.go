2c06cd8dd2fe2335409d99c7bef369816b178330
Nikolai Rodionov <allanger@badhouseplants.net>
2023-10-24 16:33:21 +0200

    Change allanger's copyright to authors

--- internal/helpers/database/database_test.go
+++ internal/helpers/database/database_test.go
@@ -1,6 +1,6 @@
 /*
  * Copyright 2021 kloeckner.i GmbH
- * Copyright 2023 Nikolai Rodionov (allanger)
+ * Copyright 2023 DB-Operator Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
