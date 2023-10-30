cf47d8b945ee4950482956b7e1a8287a329f1d72
Nikolai Rodionov <allanger@zohomail.com>
2023-10-30 20:59:07 +0100

    Add comments to Azure test compose and script

--- resources/test/az_flexible_postgres_init.sh
+++ resources/test/az_flexible_postgres_init.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+# ---------------------------------------------------------------------
+# -- Setting permissions. The "postgres" user should be the one
+# --  that has the permissions like a user provided by Azure
+# ---------------------------------------------------------------------
 export PGDATABASE=postgres
 export PGUSER=az_admin
 psql -c "CREATE USER \"postgres\" WITH PASSWORD 'test1234' CREATEDB CREATEROLE;"
@@ -7,6 +11,10 @@ psql -c "GRANT az_admin TO \"postgres\";"
 psql -c "GRANT pg_read_all_settings TO \"postgres\";"
 psql -c "GRANT pg_read_all_stats TO \"postgres\";"
 psql -c "GRANT pg_stat_scan_tables to \"postgres\";"
-# -- Azure has a complicated extensions setup, so it doesn't make sense to test them here
+# ---------------------------------------------------------------------
+# -- Azure has a complicated extensions setup,
+# --  so it doesn't make sense to test them here
+# -- Only "uuid-ossd" is tested properly by marking it as a trusted
+# ---------------------------------------------------------------------
 export PGDATABASE=template1
 psql -c "CREATE EXTENSION IF NOT EXISTS pgcrypto;"
